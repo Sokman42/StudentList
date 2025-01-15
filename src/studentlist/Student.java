7526901b7437f36e32fc0d8bcc7de7e9fddcc756
Austin Sinclair <sinclaau@sheridancollege.ca>
2025-01-15 14:11:07 -0500

    Initial Commit

---
+++ src/studentlist/Student.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author fagun at 12/05/2024
+ */
+public class Student {
+    private String name;
+    public int studentID;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void setId(int id) {
+        this.studentID = id;
+    }
+   
+}
---
+++ src/studentlist/StudentList.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author fagun
+ */
+public class StudentList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Student[] studentList = new Student[4];//change size before running
+        Student s1 = new Student();
+        s1.setName("Fagun Vankawala");
+        s1.setId(1);
+        Student s2 = new Student();
+        s2.setName("Palak Shah");
+        s2.setId(3);
+        Student s3 = new Student();
+        s3.setName("Megh Vankawala");
+        s3.setId(5);
+        Student s4 = new Student();
+        s4.setName("Austin Sinclair");
+       
+        studentList[0] = s1;
+        studentList[1] = s2;
+        studentList[2] = s3;
+        studentList[3] = s4;
+        
+        //fill in 4 student names before running
+        for (Student s : studentList) {
+            System.out.println("ID: " + s.studentID + " | Name: " + s.getName());
+        }
+
+    }
+
+}
