6253c6301a525cbf0fce3240e3d84a209137f2bb
Austin Sinclair <sinclaau@sheridancollege.ca>
2025-01-21 22:57:45 -0500

    oopsie

---
+++ src/studentlist/NewClass.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author soky4
+ */
+public class NewClass {
+    //Oops I created this by accident haha
+    
+    
+}
