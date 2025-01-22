4ee753b8bf303448957b9e9417c9decfcfc9dc55
Austin Sinclair <sinclaau@sheridancollege.ca>
2025-01-21 22:56:17 -0500

    New branch to test out Course class with enroll and print methods.

---
+++ src/studentlist/Course.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author soky4
+ */
+public class Course {
+    private final Student[] students;
+    
+    public Course() {
+        this.students = new Student[10];
+}
+    
+    public void enrollStudent(Student s1) {
+        for (int s = 0; s <= students.length - 1; s++) {
+            if (students[s] == null) {
+                this.students[s] = s1;
+            }
+        }
+    }
+    
+    public void printClass() {
+        for (Student s : students) {
+            System.out.print("Student Name: " + s.getName());
+            System.out.print("Student ID: " + s.getId());
+            System.out.print("Student Address: " + s.getAddress());
+        }
+        
+    }
+    
+}
